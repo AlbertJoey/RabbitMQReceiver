c6ca754ab19aa83b51989aa233cf5308fad8dbda
AlbertJoey <cheng.jun@Outlook.com>
2016-05-27 11:42:34 +0800

    update commit

--- testRabbitMQReciver/Program.cs
+++ testRabbitMQReciver/Program.cs
@@ -119,7 +119,7 @@ namespace testRabbitMQReciver
             //                            type: "direct");
             //    var queueName = channel.QueueDeclare().QueueName;
 
-               
+
             //    args = new string[] { "error", "info", "warning" };
 
             //    foreach (var severity in args)
@@ -150,50 +150,106 @@ namespace testRabbitMQReciver
             #endregion
 
             #region Topics
+            //var factory = new ConnectionFactory() { HostName = "192.168.100.37", Password = "Git123!@#", UserName = "GitAdmin", Port = 5672 };
+            //using (var connection = factory.CreateConnection())
+            //using (var channel = connection.CreateModel())
+            //{
+            //    channel.ExchangeDeclare(exchange: "topic_logs", type: "topic");
+            //    var queueName = channel.QueueDeclare().QueueName;
+
+            //    //args = new string[] { "*.orange.*" };
+            //    args = new string[] { "*.*.rabbit", "lazy.#" };
+
+            //    foreach (var bindingKey in args)
+            //    {
+            //        channel.QueueBind(queue: queueName,
+            //                          exchange: "topic_logs",
+            //                          routingKey: bindingKey);
+            //    }
+
+            //    Console.WriteLine(" [*] Waiting for messages. To exit press CTRL+C");
+
+            //    var consumer = new EventingBasicConsumer(channel);
+            //    consumer.Received += (model, ea) =>
+            //    {
+            //        var body = ea.Body;
+            //        var message = Encoding.UTF8.GetString(body);
+            //        var routingKey = ea.RoutingKey;
+            //        Console.WriteLine(" [x] Received '{0}':'{1}'",
+            //                          routingKey,
+            //                          message);
+            //    };
+            //    channel.BasicConsume(queue: queueName,
+            //                         noAck: true,
+            //                         consumer: consumer);
+
+            //    Console.WriteLine(" Press [enter] to exit.");
+            //    Console.ReadLine();
+            //}
+            #endregion
+
+            #region RPC
             var factory = new ConnectionFactory() { HostName = "192.168.100.37", Password = "Git123!@#", UserName = "GitAdmin", Port = 5672 };
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
             {
-                channel.ExchangeDeclare(exchange: "topic_logs", type: "topic");
-                var queueName = channel.QueueDeclare().QueueName;
-
-                if (args.Length < 1)
-                {
-                    Console.Error.WriteLine("Usage: {0} [binding_key...]",
-                                            Environment.GetCommandLineArgs()[0]);
-                    Console.WriteLine(" Press [enter] to exit.");
-                    Console.ReadLine();
-                    Environment.ExitCode = 1;
-                    return;
-                }
-
-                foreach (var bindingKey in args)
-                {
-                    channel.QueueBind(queue: queueName,
-                                      exchange: "topic_logs",
-                                      routingKey: bindingKey);
-                }
-
-                Console.WriteLine(" [*] Waiting for messages. To exit press CTRL+C");
-
-                var consumer = new EventingBasicConsumer(channel);
-                consumer.Received += (model, ea) =>
-                {
-                    var body = ea.Body;
-                    var message = Encoding.UTF8.GetString(body);
-                    var routingKey = ea.RoutingKey;
-                    Console.WriteLine(" [x] Received '{0}':'{1}'",
-                                      routingKey,
-                                      message);
-                };
-                channel.BasicConsume(queue: queueName,
-                                     noAck: true,
+                channel.QueueDeclare(queue: "rpc_queue",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+                channel.BasicQos(0, 1, false);
+                var consumer = new QueueingBasicConsumer(channel);
+                channel.BasicConsume(queue: "rpc_queue",
+                                     noAck: false,
                                      consumer: consumer);
+                Console.WriteLine(" [x] Awaiting RPC requests");
 
-                Console.WriteLine(" Press [enter] to exit.");
-                Console.ReadLine();
+                while (true)
+                {
+                    string response = null;
+                    var ea = (BasicDeliverEventArgs)consumer.Queue.Dequeue();
+
+                    var body = ea.Body;
+                    var props = ea.BasicProperties;
+                    var replyProps = channel.CreateBasicProperties();
+                    replyProps.CorrelationId = props.CorrelationId;
+
+                    try
+                    {
+                        var message = Encoding.UTF8.GetString(body);
+                        int n = int.Parse(message);
+                        Console.WriteLine(" [.] fib({0})", message);
+                        response = fib(n).ToString();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(" [.] " + e.Message);
+                        response = "";
+                    }
+                    finally
+                    {
+                        var responseBytes = Encoding.UTF8.GetBytes(response);
+                        channel.BasicPublish(exchange: "",
+                                             routingKey: props.ReplyTo,
+                                             basicProperties: replyProps,
+                                             body: responseBytes);
+                        channel.BasicAck(deliveryTag: ea.DeliveryTag,
+                                         multiple: false);
+                    }
+                }
             }
             #endregion
         }
+        private static int fib(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return n;
+            }
+
+            return fib(n - 1) + fib(n - 2);
+        }
     }
+    
 }
